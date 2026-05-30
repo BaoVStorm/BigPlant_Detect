23522e1192fdea4e9b4a2cb74390260e6b48c6ca
VStorm <tranvubao2004@gmail.com>
2026-05-30 20:25:59 +0700

    feat: none

--- app/services/device.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def choose_device(device_arg: str) -> torch.device:
-    if device_arg == "auto":
-        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    return torch.device(device_arg)
